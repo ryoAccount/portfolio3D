d62419b71b9502c5d1475a7d9072d118c07593c5
ryoAccount <jane.doe3wtp4649@gmail.com>
2024-02-26 02:16:57 +0900

    to 1:13:00

---
+++ src/models/Plane.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import planeScene from "../assets/3d/plane.glb";
+import { useGLTF } from '@react-three/drei';
+
+function Plane({ isRotaing, ...props }) {
+  const { scene, animations } = useGLTF(planeScene);
+
+  return (
+    <mesh {...props}>
+      <primitive object={scene} />
+    </mesh>
+
+  )
+}
+
+export default Plane
