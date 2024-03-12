b326e7499eb3cd0c83e737d961b7bdf50de10a38
ryoAccount <jane.doe3wtp4649@gmail.com>
2024-03-13 02:00:13 +0900

    library update & eslint

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -20,8 +20,6 @@ export function Sky({ isRotating }) {
 
   return (
     <mesh ref={skyRef}>
-      // use the primitive element when you want to directly embed a complex 3D
-      model or scene
       <primitive object={sky.scene} />
     </mesh>
   );
